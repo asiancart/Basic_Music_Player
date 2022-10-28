9704603e3e06aa56786ed777a3110abe999c974e
ilyasss <ex_p@live.com>
2022-10-28 13:44:28 +0300

    youtube video downloader

--- main.py
+++ main.py
@@ -1,61 +1,18 @@
+#need to import pytube
+from pytube import YouTube
 
-from tkinter import *
-import pyperclip
-import random
+#ask for the link from user
+link = input("Enter the link of YouTube video you want to download:  ")
+yt = YouTube(link)
 
+#Showing details
+print("Title: ",yt.title)
+print("Number of views: ",yt.views)
+print("Length of video: ",yt.length)
+print("Rating of video: ",yt.rating)
 
-root = Tk()
-root.geometry("400x400")
+#Getting the highest resolution possible and downloading
 
-# declaring a variable of string type and this variable will be
-# used to store the password generated
-passstr = StringVar()
+yt.streams.get_highest_resolution().download()
 
-# declaring a variable of integer type which will be used to
-# store the length of the password entered by the user
-passlen = IntVar()
-
-# setting the length of the password to zero initially
-passlen.set(0)
-
-def generate():
-    pass1 = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
-            'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
-            'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D',
-            'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
-            'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
-            'Y', 'Z', '1', '2', '3', '4', '5', '6', '7', '8',
-            '9', '0', ' ', '!', '@', '#', '$', '%', '^', '&',
-            '*', '(', ')']
-
-    password = ""
-
-    for x in range(passlen.get()):
-        password = password + random.choice(pass1)
-    passstr.set(password)
-def copytoclipboard():
-    random_password = passstr.get()
-    pyperclip.copy(random_password)
-
-# Creating a text label widget
-Label(root, text="Password Generator Application", font="calibri 20 bold").pack()
-
-# Creating a text label widget
-Label(root, text="Enter password length").pack(pady=3)
-
-# Creating a entry widget to take password length entered by the
-# user
-Entry(root, textvariable=passlen).pack(pady=3)
-
-# button to call the generate function
-Button(root, text="Generate Password", command=generate).pack(pady=7)
-
-# entry widget to show the generated password
-Entry(root, textvariable=passstr).pack(pady=3)
-
-# button to call the copytoclipboard function
-Button(root, text="Copy to clipboard", command=copytoclipboard).pack()
-
-# mainloop() is an infinite loop used to run the application when
-# it's in ready state
-root.mainloop()
+print("Download completed!!")
