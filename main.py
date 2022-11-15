629ff0e19f33e2d7823dea7594d02600d2b7887f
ilyasss <ex_p@live.com>
2022-11-15 20:07:56 +0300

    basic music player

--- main.py
+++ main.py
@@ -1,46 +1,24 @@
-gimport qrcode
-from tkinter import *
+import pygame
+from pygame import mixer
 
-cp = Tk()
-cp.title('github.com/asiancart')
-cp.geometry('700x250')
-cp.config(bg='#e52165')
+pygame.init()
+mixer.init()
+screen = pygame.display.set_mode((600, 400))
+mixer.music.load("filename.mp3")
+mixer.music.play()
 
-def generate():
-    img = qrcode.make(msg.get())
-    type(img)
-    img.save(f'{save_name.get()}.png')
-    Label(cp, text='File Saved!', bg='#e52165' , fg='black', font=('Arial Black', 8)).pack()
-
-def show():
-    img = qrcode.make(msg.get())
-    type(img)
-    img.show()
-
-frame = Frame(cp, bg='#e52165')
-frame.pack(expand=True)
-
-#------------------ENTER THE TEXT OR URL------------------
-
-Label(frame, text='Enter the Text or URL : ', font=('Arial Black', 16),
-      bg='#e52165').grid(row=0, column=0, sticky='w')
-
-msg = Entry(frame)
-msg.grid(row=0, column=1)
-
-#------------------ENTER THE FILE NAME------------------
-
-Label(frame, text='File Name(Save As) : ', font=('Arial Black', 16),
-      bg='#e52165').grid(row=1, column=0, sticky='w')
-
-save_name = Entry(frame)
-save_name.grid(row=1, column=1)
-
-#------------------BUTTONS TO SHOW OR SAVE QRCODE------------------
-
-btn = Button(cp, text='Show QR code', bd='5', command=show, width=15)
-btn.pack()
-btn = Button(cp, text='Save QR code', command=generate, bd='5', width=15)
-btn.pack()
-
-cp.mainloop()
+print("Press 'p' to pause 'r' to resume")
+print("Press 'q' to quit")
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_p:
+                mixer.music.pause()
+            if event.key == pygame.K_r:
+                mixer.music.unpause()
+            if event.key == pygame.K_q:
+                running = False
+quit()
